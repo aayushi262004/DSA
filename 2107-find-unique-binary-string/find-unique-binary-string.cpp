ce362c7dbc6490764258325f861483afc7d68f91
Aayushi Gupta <aayushi262004@gmail.com>
2025-02-20 19:39:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (84.00%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        int len=nums[0].size();
-        int n=pow(2,len);
-        unordered_map<int,int>hashTable;
-       
-        for(int i=0;i<n;i++){
-              hashTable[i] = i;
+       string result;
+       int n=nums.size();
+       for(int i=0;i<n;i++){
+        if(nums[i][i]=='0'){
+            result+='1';
+        }else{
+            result+='0';
         }
-         for(int i=0;i<n;i++){
-             string num = bitset<16>(hashTable[i]).to_string().substr(16 - len);
-              bool flag=true;
-        for(int j=0;j<nums.size();j++){
-                 if(num == nums[j]){   
-                   flag=false;
-                   break;
-                 }
-            }
-            if(flag==true){
-                return num;
-            }
-        }
-      return "";
+       }
+       return result;
     }
 };
