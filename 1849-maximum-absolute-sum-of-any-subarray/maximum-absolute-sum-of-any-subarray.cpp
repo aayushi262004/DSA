e9634abf57728e293e581bff90162c737e1257ab
Aayushi Gupta <aayushi262004@gmail.com>
2025-02-26 12:50:32 +0530

    Update maximum-absolute-sum-of-any-subarray.cpp

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -1,3 +1,41 @@
+//better approach
+
+class Solution {
+    int maxi=INT_MIN;
+    int max_subarray(vector<int> &nums,int &n){
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            maxi=max(sum,maxi);
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return maxi;
+    }
+   int min_subarray(vector<int> &nums,int &n){
+    int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=(nums[i]*-1);
+            maxi=max(sum,maxi);
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return maxi;
+    }
+public:
+    int maxAbsoluteSum(vector<int>& nums) {
+        int n=nums.size();
+        int val1=max_subarray(nums,n);
+        int val2=min_subarray(nums,n);
+        int res=max(val1,val2);
+        return res;
+    }
+};
+
+
+//optimal approach
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
