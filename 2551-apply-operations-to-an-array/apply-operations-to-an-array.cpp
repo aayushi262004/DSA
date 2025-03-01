f16d1b9cde8dd359bd29eb58543dad6519c7a99a
Aayushi Gupta <aayushi262004@gmail.com>
2025-03-01 16:10:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (87.59%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -8,13 +8,12 @@ public:
                 nums[i+1]=0;
             }
         }
-        vector<int>arr(n,0);
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0){
-                arr[cnt++]=nums[i];
-            }
+       int cnt=0;
+       for(int i=0;i<n;i++){
+        if(nums[i]!=0){
+            swap(nums[cnt++],nums[i]);
         }
-        return arr;
+       }
+               return nums;
     }
 };
