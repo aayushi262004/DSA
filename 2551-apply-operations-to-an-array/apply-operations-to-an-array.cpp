02ca9a845f4c3e31cc8d29e1e981f672c633689d
Aayushi Gupta <aayushi262004@gmail.com>
2025-03-01 16:18:50 +0530

    Update apply-operations-to-an-array.cpp

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -1,3 +1,26 @@
+//brute force solution
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]*=2;
+                nums[i+1]=0;
+            }
+        }
+        vector<int>arr(n,0);
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                arr[cnt++]=nums[i];
+            }
+        }
+        return arr;
+    }
+};
+
+//after removing space complexity
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
