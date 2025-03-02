25d04a36bab9d7214bb9423edb68888b508c09f5
Aayushi Gupta <aayushi262004@gmail.com>
2025-03-02 16:58:01 +0530

    Update single-number.cpp

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,3 +1,26 @@
+
+//brute force approach
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int cnt=0;
+            int val= nums[i];
+            for(int j=0;j<n;j++){
+                if(val==nums[j]){
+                    cnt++;
+                }
+            }
+            if(cnt<2){
+                ans= val;
+            }
+        }
+        return ans;
+    }
+};
+//optimal approach
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
