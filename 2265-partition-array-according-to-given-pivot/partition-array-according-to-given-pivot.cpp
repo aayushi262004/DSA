0dccffa461c4999e1ad4dba67ad286bc85763d1b
Aayushi Gupta <aayushi262004@gmail.com>
2025-03-03 23:03:52 +0530

    Update partition-array-according-to-given-pivot.cpp

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,3 +1,6 @@
+//approach-2
+//TC:O(n)+O(n)=O(n);
+//SC:O(n);
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
@@ -19,5 +22,31 @@ public:
         low++;
       }
       return res;
+    }
+};
+
+
+//approach-1
+// TC:O(n+n)=O(n);
+//SC:O(n);
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        int n=nums.size();
+       vector<int>small;
+       vector<int>pivott;
+       vector<int>large;
+       for(int i=0;i<n;i++){
+        if(nums[i]<pivot){
+            small.push_back(nums[i]);
+        }else if (nums[i]==pivot){
+            pivott.push_back(nums[i]);
+        }else{
+            large.push_back(nums[i]);
+        }
+       } 
+       small.insert(small.end(),pivott.begin(),pivott.end());
+       small.insert(small.end(),large.begin(),large.end());
+       return small;
     }
 };
