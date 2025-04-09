7b4c224c43185b3072b90767ed1285f17de8b2db
Aayushi Gupta <aayushi262004@gmail.com>
2025-04-09 19:43:46 +0530

    Update intersection-of-two-linked-lists.cpp

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -1,3 +1,5 @@
+BRUTE FORCE
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -21,5 +23,32 @@ public:
             headA=headA->next;
         }
         return NULL;
+    }
+};
+
+USING HASHMAP
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_map<ListNode* ,int>mpp;
+        while(headA!=NULL){
+            mpp[headA]++;
+            headA=headA->next;
+        }
+        while(headB!=NULL){
+            if(mpp[headB]>0){
+                return headB;
+            }
+            headB=headB->next;
+        }
+    return NULL;
     }
 };
