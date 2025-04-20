538371f1711f8bac56bf7af3e6b7e2822a6222a1
Aayushi Gupta <aayushi262004@gmail.com>
2025-04-20 15:26:48 +0530

    Time: 4 ms (70.30%) | Memory: 119.7 MB (86.85%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -1,20 +1,16 @@
 class Solution {
-    #define ll long long
-
-    ll findMaxPoints(vector<vector<int>>& questions,int pos,vector<ll>& mem){
-        if(pos >= questions.size())
-            return 0;
-        if(mem[pos]!=-1)
-            return mem[pos];
-        
-        ll exclude = findMaxPoints(questions,pos+1,mem);
-        ll include = questions[pos][0] + findMaxPoints(questions,pos+questions[pos][1]+1,mem);
-        return mem[pos] = max(exclude,include);
-    }
 public:
     long long mostPoints(vector<vector<int>>& questions) {
-        int n = questions.size();
-        vector<ll> mem(n,-1);
-        return findMaxPoints(questions,0,mem);
+        int n=questions.size();
+        vector<long long>dp(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            int val=questions[i][0];
+            int next=(i+questions[i][1]+1);
+            long long take=val+(next<n? dp[next]:0);
+            long long dont_take=dp[i+1]; 
+            dp[i]=max(take,dont_take);
+        }
+        return dp[0];
+
     }
 };
