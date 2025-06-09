5de52e9cc338430820beaf88877dace30cfbab1f
Aayushi Gupta <aayushi262004@gmail.com>
2025-06-09 21:59:10 +0530

    Time: 5 ms (1.36%) | Memory: 16 MB (8.99%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
+    void partition(vector<int> &nums, int low, int high){
+        if(low<high){
+            int mid=(low+high)/2;
+            partition(nums,low,mid);
+            partition(nums,mid+1,high);
+            mergeSort(nums,low,mid,high);
+        }
+    }
+    void mergeSort(vector<int>&nums,int low, int mid, int high){
+       int i=low;
+       int j=mid+1;
+       vector<int>temp;
+       while(i<=mid && j<=high){
+        if(nums[i]<nums[j]){
+            temp.push_back(nums[i]);
+            i++;
+        }else{
+            temp.push_back(nums[j]);
+            j++;
+        }
+       }
+        while(i<=mid){
+            temp.push_back(nums[i]);
+            i++;
+        }
+        while(j<=high){
+            temp.push_back(nums[j]);
+            j++;
+        }
+        for(int k=0;k<temp.size();k++){
+            nums[low+k]=temp[k];
+        }
+       } 
+    
+     
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m-1;
-        int j=n-1;
-        int k=m+n-1;
-        while(i>=0 && j>=0){
-            if(nums1[i]> nums2[j]){
-                nums1[k]=nums1[i];
-                i--;
-                k--;
-            }else{
-                nums1[k]=nums2[j];
-                j--;
-                k--;
-            }
-        }
-        while(j>=0){
-            nums1[k]=nums2[j];
-            j--;
-            k--;
-        }
+       for(int i=0;i<n;i++){
+            nums1[m+i]=nums2[i];
+       }
+       partition(nums1,0,m+n-1);
+      
     }
 };
