1b58709944a822596658bbe59466129dd42d2d2f
Aayushi Gupta <aayushi262004@gmail.com>
2025-06-30 16:37:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (76.46%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
-    int kadane_max(vector<int>& nums, int n){
+    int kadanemax(vector<int> &nums, int n){
         int curr_sum=nums[0];
         int max_sum=nums[0];
         for(int i=1;i<n;i++){
-            curr_sum = max(nums[i],curr_sum+nums[i]);
-            max_sum = max(curr_sum,max_sum);
-
+            curr_sum=max(nums[i],curr_sum+nums[i]);
+            max_sum=max(curr_sum,max_sum);
         }
         return max_sum;
     }
-
-    int kadane_min(vector<int>& nums, int n){
+    int kadanemin(vector<int>&nums, int n){
         int curr_sum=nums[0];
         int min_sum=nums[0];
         for(int i=1;i<n;i++){
-            curr_sum = min(nums[i],curr_sum+nums[i]);
-            min_sum = min(curr_sum,min_sum);
-
+            curr_sum=min(nums[i],curr_sum+nums[i]);
+            min_sum=min(min_sum,curr_sum);
         }
-        return min_sum;
+    return min_sum;
     }
     int maxSubarraySumCircular(vector<int>& nums) {
+        int sum=accumulate(nums.begin(),nums.end(),0);
         int n=nums.size();
-        int sum = accumulate(nums.begin(),nums.end(),0);
-
-        int MaxSum = kadane_max(nums,n);
-        int MinSum = kadane_min(nums,n);
-
-        int CircularSum = sum-MinSum;
-
-        if(MaxSum>0){
-            return max(MaxSum, CircularSum);
+        int maxsubarray=kadanemax(nums,n);
+        int minsubarray=kadanemin(nums,n);
+        int circular=sum-minsubarray;
+        if(maxsubarray>0){
+            return max(circular,maxsubarray);
         }
-    return MaxSum;
+        return maxsubarray;
     }
 };
