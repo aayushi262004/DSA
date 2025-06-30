97f23c4d644587daf70cd00b76ffe569f299e062
Aayushi Gupta <aayushi262004@gmail.com>
2025-06-30 17:06:02 +0530

    Time: 12 ms (85.55%) | Memory: 24.8 MB (84.94%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>>mpp;
+        unordered_map<string,vector<string>>vec;
         for(auto c:strs){
             string curr=c;
             sort(c.begin(),c.end());
-            mpp[c].push_back(curr);
+            vec[c].push_back(curr);
         }
-        vector<vector<string>>result;
-        for(auto &pair:mpp){
-            result.push_back(pair.second);
+        vector<vector<string>>res;
+        for(auto &pair: vec){
+            res.push_back(pair.second);
         }
-                
-    return result;
+    return res;
     }
 };
