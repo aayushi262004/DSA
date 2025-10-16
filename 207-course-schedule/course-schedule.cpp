96a4f9077fba8fa2e1d588749cd3efcfd395150c
Aayushi Gupta <aayushi262004@gmail.com>
2025-10-16 21:44:13 +0530

    Time: 7 ms (48.30%) | Memory: 20.1 MB (31.03%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     bool canFinish(int V, vector<vector<int>>& prerequisites) {
-     vector<vector<int>>adjls(V);
-     for(auto it:prerequisites){
-        adjls[it[1]].push_back(it[0]);
-     }  
-     vector<int>indegree(V,0);
-     for(int i=0;i<V;i++){
-        for(auto it:adjls[i]){
+        vector<vector<int>>adjls(V);
+        for(auto it:prerequisites){
+            adjls[it[0]].push_back(it[1]);
+        }
+        vector<int>indegree(V,0);
+        for(int i=0;i<V;i++){
+            for(auto it:adjls[i]){
             indegree[it]++;
+            }
         }
-     }
-     queue<int>q;
-
-     for(int i=0;i<V;i++){
-        if(indegree[i]==0){
-            q.push(i);
+        queue<int>q;
+        int cnt=0;
+        vector<int>topo;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
         }
-     } 
-     vector<int>topo;
-     int cnt=0;
-     while(!q.empty()){
-        int node=q.front();
-        q.pop();
-        topo.push_back(node);
-        cnt++;
-        for(auto it:adjls[node]){
-            indegree[it]--;
-            if(indegree[it]==0)q.push(it);
-        }
-     }
-    return cnt==V;
+            while(!q.empty()){
+                    int val=q.front();
+                    q.pop();
+                    cnt++;
+                    for(auto it:adjls[val]){
+                        indegree[it]--;
+                    
+                    if(indegree[it]==0){
+                        q.push(it);
+                    }
+                }
+                }
+            
+        return cnt==V;
     }
 };
