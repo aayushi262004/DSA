15d4bbc44905dc83900fc4fb3443e2ad18b8f673
Aayushi Gupta <aayushi262004@gmail.com>
2025-10-24 09:59:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (19.01%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,39 +1,52 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int tot=0, days=0;
-        int cnt=0;
         if(grid.empty())    return 0;
-       //vector<vector<int>>adj;
-       queue<pair<int,int>>rotten;
-       for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(grid[i][j]!=0)   tot++;
-            if(grid[i][j]==2)   rotten.push({i,j});
-        }
-       }
-       int dx[4]={0,0,-1,1};
-       int dy[4]={1,-1,0,0};
-
-       while(!rotten.empty()){
-        int k=rotten.size();
-         cnt+=k;
-        while(k--){
-            int x=rotten.front().first,y=rotten.front().second;
-            rotten.pop();
-            for(int i=0;i<4;i++){
-                int nx=x+dx[i];
-                int ny=y+dy[i];
-                if(nx<0 ||ny<0 || nx>=n || ny>=m || grid[nx][ny]!=1)continue;
-                grid[nx][ny]=2;
-                rotten.push({nx,ny});
+        int total=0, rotten=0;
+        int m=grid.size();
+        int n=grid[0].size();
+        queue<pair<int,int>>q;
+        vector<vector<int>>image=grid;
+        vector<vector<int>>vis(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]!=0){
+                    total++;
+                }
+                if(grid[i][j]==2){
+                    rotten++;
+                    q.push({i,j});
+                    vis[i][j]=1;
+                }
             }
         }
-        if(!rotten.empty())days++;
-       }
-       return tot==cnt?days:-1;
-
+        int dx[]={0,0,1,-1};
+        int dy[]={1,-1,0,0};
+        int day=0;
+        
+        while(!q.empty()){
+            int s=q.size();
+            bool changed=false;
+            for(int j=0;j<s;j++){
+                int row=q.front().first;
+                int col=q.front().second;
+                q.pop();
+                for(int i=0;i<4;i++){
+                    int nrow=row+dx[i];
+                    int ncol=col+dy[i];
+            
+            if(nrow<m && nrow>=0 && ncol<n && ncol>=0 && image[nrow][ncol]==1 &&  vis[nrow][ncol]!=1){
+                image[nrow][ncol]=2;
+                rotten++;
+                vis[nrow][ncol]=1;
+                q.push({nrow,ncol});
+                changed=true;
+            }
+            }
+            }
+          if(changed==true)day++; 
+        }
+        
+    return(total==rotten?day:-1);
     }
 };
