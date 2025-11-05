ef3392f9a480cee7520e0354e28e2ecbf5e0b043
Aayushi Gupta <aayushi262004@gmail.com>
2025-11-06 00:50:28 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (33.18%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -3,31 +3,29 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int m=image.size();
         int n=image[0].size();
-        queue<pair<int,int>>q;
+      
         int initColor=image[sr][sc];
-        if(initColor==color)return image;
+          if(initColor==color) return image;
+        queue<pair<int,int>>q;
         image[sr][sc]=color;
-        //vector<vector<int>>cpp=image;
         q.push({sr,sc});
-        
         int dx[]={0,0,1,-1};
         int dy[]={1,-1,0,0};
         while(!q.empty()){
-            //int s=q.size();
-            //for(int i=0;i<s;i++){
-                int row=q.front().first;
-                int col=q.front().second;
-                q.pop();
-                for(int j=0;j<4;j++){
-                    int nrow=row+dx[j];
-                    int ncol=col+dy[j];
-                    if(nrow<m &&nrow>=0 && ncol<n && ncol>=0 && image[nrow][ncol]==initColor){
-                        image[nrow][ncol]=color;
-                        q.push({nrow,ncol});
-                    }
+            int s=q.size();
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=row+dx[i];
+                int ncol=col+dy[i];
+
+                if(nrow>=0 && ncol>=0 && nrow<m && ncol<n && image[nrow][ncol]==initColor){
+                    image[nrow][ncol]=color;
+                    q.push({nrow,ncol});
                 }
             }
-        
-     return image;   
+        }
+    return image;
     }
 };
