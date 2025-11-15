8c1a46333eddd6bd7c316e5daee7d544e12d379c
Aayushi Gupta <aayushi262004@gmail.com>
2025-11-15 16:43:46 +0530

    Time: 7 ms (44.85%) | Memory: 10.9 MB (69.70%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int left=0;
-        int right=0;
-        int maxlen=0;
-        int maxfreq=0;
-        unordered_map<char,int>mpp{};
-        while(right<s.size()){
-            mpp[s[right]-'A']++;
-            maxfreq=max(maxfreq,mpp[s[right]-'A']);
-            while((right-left+1)-maxfreq>k){
-               maxfreq=0;
-               mpp[s[left]-'A']--;
-               for(int i=0;i<26;i++) 
-               maxfreq=max(maxfreq,mpp[i]);
-                left++;
+        int i = 0;
+        int ans = 0;
+        int n = s.length();
+        int maxFreq = 0;
+        unordered_map<char,int> freq;
+
+        for (int j = 0; j < n; j++) {
+
+            freq[s[j]]++;                     // FIX 1
+            maxFreq = max(maxFreq, freq[s[j]]); // track most frequent char
+
+            // If window needs more than k replacements → shrink
+            while ((j - i + 1) - maxFreq > k) {   // FIX 2
+                freq[s[i]]--;
+                i++;
             }
-            if((right-left+1)-maxfreq<=k){
-                maxlen=max(maxlen,right-left+1);
-                right++;
-            }
+
+            ans = max(ans, j - i + 1);
         }
-        return maxlen;
-    }
+
+        return ans;
+    }
 };
