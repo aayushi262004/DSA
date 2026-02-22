e48c90f1745a7ea832e3d8cee15ab75edc188280
Aayushi Gupta <aayushi262004@gmail.com>
2026-02-22 14:12:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (94.66%) - LeetSync

---
+++ 899-binary-gap/binary-gap.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int start=-1;
+        int end=-1;
+        int ans=0;
+        for(int i=0;i<32;i++){
+            int d=(n>>i)&1;
+            if(d){
+                start=(start==-1)?i:start;
+                end=(end==-1)?start:i;
+                ans=max(ans,end-start);
+                start=end;
+            }
+        }
+    return ans;
+    }
+};
