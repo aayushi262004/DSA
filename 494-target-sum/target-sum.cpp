25f92484387ea1d61c5ba34f1ec17a695d6c0e6e
Aayushi Gupta <aayushi262004@gmail.com>
2026-02-22 23:33:36 +0530

    Time: 3 ms (86.56%) | Memory: 15.6 MB (41.64%) - LeetSync

---
+++ 494-target-sum/target-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        if (abs(target) > totalSum || (totalSum + target) % 2 != 0) return 0;
+
+        int sum = (totalSum + target) / 2;
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
+
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                if (nums[i - 1] <= j) {
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+};
