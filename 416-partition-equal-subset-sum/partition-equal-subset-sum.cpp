4ef142e86d5e16a78ec172b72eb11411884a156a
Aayushi Gupta <aayushi262004@gmail.com>
2026-03-23 23:53:25 +0530

    Time: 126 ms (57.52%) | Memory: 17.5 MB (59.77%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    bool subset_sum(vector<int>& nums, int sum) {
+        int n = nums.size();
+        vector<vector<bool>> dp(n+1, vector<bool>(sum+1, false));
+
+        // base case
+        for(int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= sum; j++) {
+                if(nums[i-1] <= j) {
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j - nums[i-1]];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][sum];
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
+        if(sum % 2 != 0) return false;
+
+        return subset_sum(nums, sum / 2);
+    }
+};
