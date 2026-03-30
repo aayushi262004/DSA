29f66bfbf7c9feb5b9d9f87726aa988fbda1d4d0
Aayushi Gupta <aayushi262004@gmail.com>
2026-03-30 23:00:50 +0530

    Time: 134 ms (48.44%) | Memory: 17.5 MB (59.30%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,36 +1,27 @@
-#include <vector>
-#include <numeric>
-using namespace std;
-
 class Solution {
 public:
-    bool subset_sum(vector<int>& nums, int sum) {
-        int n = nums.size();
-        vector<vector<bool>> dp(n+1, vector<bool>(sum+1, false));
-
-        // base case
-        for(int i = 0; i <= n; i++) {
-            dp[i][0] = true;
+    bool solve(vector<int>& nums, int sum){
+        int n=nums.size();
+        vector<vector<bool>>dp(n+1,vector<bool>(sum+1, false));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=true;
         }
-
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= sum; j++) {
-                if(nums[i-1] <= j) {
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j - nums[i-1]];
-                } else {
-                    dp[i][j] = dp[i-1][j];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum;j++){
+                if(nums[i-1]<=j){
+                    dp[i][j]= dp[i-1][j-nums[i-1]]||dp[i-1][j];
+                }else{
+                    dp[i][j]=dp[i-1][j];
                 }
             }
         }
-
-        return dp[n][sum];
+    return dp[n][sum];
     }
-
     bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-
-        if(sum % 2 != 0) return false;
-
-        return subset_sum(nums, sum / 2);
+        int total=0;
+        total=accumulate(nums.begin(),nums.end(),0);
+        if(total%2 !=0) return false;
+        
+        return solve(nums, total/2);
     }
 };
