f1e898eab5c31c048b2ab3ca613d2344f6e2f18f
Aayushi Gupta <aayushi262004@gmail.com>
2026-04-03 01:08:23 +0530

    Time: 20 ms (48.79%) | Memory: 58.1 MB (57.83%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -9,43 +9,20 @@
  * };
  */
 class Solution {
-    ListNode* mergeSortll(ListNode* list1,ListNode* list2){
-        ListNode* dummyNode= new ListNode(-1);
-        ListNode* temp=dummyNode;
-        while(list1 != nullptr && list2!= nullptr){
-            if(list1->val<=list2->val){
-                temp->next=list1;
-                list1=list1->next;
-            }else{
-                temp->next=list2;
-                list2=list2->next;
-            }
-            temp=temp->next;
-        }
-        if(list1!=nullptr){
-            temp->next=list1;
-        }else{
-            temp->next=list2;
-        }
-        return dummyNode->next;
-    }
 public:
     ListNode* sortList(ListNode* head) {
-        if(head==NULL || head->next==NULL){
-            return head;
+        ListNode *temp=head;
+        priority_queue<int, vector<int>,greater<int>>pq;
+        while(temp!= NULL){
+            pq.push(temp->val);
+            temp=temp->next;
         }
-        ListNode* fast=head;
-        ListNode* slow=head;
-        while(fast->next!=NULL && fast->next->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
+        temp=head;
+        while(temp != NULL){
+            temp->val=pq.top();
+            temp=temp->next;
+            pq.pop();
         }
-        ListNode*right=slow->next;
-        slow->next=NULL;
-        ListNode*left=head;
-        left=sortList(left);
-        right=sortList(right);
-        return mergeSortll(left,right);
-        
+    return head;
     }
 };
