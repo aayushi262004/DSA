2219b12a5e158554f5cbb431059a350646d3de42
Aayushi Gupta <aayushi262004@gmail.com>
2026-04-10 21:30:59 +0530

    Time: 63 ms (6.02%) | Memory: 75.5 MB (37.64%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,19 +10,51 @@
  */
 class Solution {
 public:
+    ListNode* findMiddle(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+
+        while(list1 != NULL && list2 != NULL){
+            if(list1->val < list2->val){   // ✅ val instead of data
+                temp->next = list1;
+                temp = list1;
+                list1 = list1->next;
+            } else {
+                temp->next = list2;
+                temp = list2;
+                list2 = list2->next;
+            }
+        }
+
+        if(list1) temp->next = list1;
+        else temp->next = list2;
+
+        return dummyNode->next;
+    }
+
     ListNode* sortList(ListNode* head) {
-        ListNode *temp=head;
-        priority_queue<int, vector<int>,greater<int>>pq;
-        while(temp!= NULL){
-            pq.push(temp->val);
-            temp=temp->next;
-        }
-        temp=head;
-        while(temp != NULL){
-            temp->val=pq.top();
-            temp=temp->next;
-            pq.pop();
-        }
-    return head;
+        if(head == NULL || head->next == NULL)
+            return head;
+
+        ListNode* middle = findMiddle(head);
+        ListNode* right = middle->next;
+        middle->next = NULL;
+        ListNode* left = head;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        return mergeTwoLists(left, right);
     }
 };
