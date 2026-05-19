7baef56fc2aeddb7ee9d540eb377ed721337bfa8
Aayushi Gupta <aayushi262004@gmail.com>
2026-05-19 22:09:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (15.83%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool allcapital(string word){
+        for(auto& ch: word){
+            if(ch<'A'|| ch>'Z'){
+                return false;
+            }
+        }
+    return true;
+    }
+    bool allsmall(string word){
+        for(auto& ch:word){
+            if(ch<'a' || ch>'z'){
+                return false;
+            }
+        }
+    return true;
+    }
+    bool detectCapitalUse(string word) {
+        if(allcapital(word) || allsmall(word) ||allsmall(word.substr(1))){
+            return true;
+        }
+    return false;
+    }
+};
