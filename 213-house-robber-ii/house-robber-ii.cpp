188b3936c1f66c4ecb1a7968500dfd10314f5256
Aayushi Gupta <aayushi262004@gmail.com>
2026-05-22 19:41:12 +0530

    Time: 1 ms (11.06%) | Memory: 10.9 MB (36.22%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -7,41 +7,33 @@ public:
         if(n == 1)
             return nums[0];
 
-        vector<int> dp1(n, 0);
-        vector<int> dp2(n, 0);
+        vector<int> dp1(n + 1, 0);
+        vector<int> dp2(n + 1, 0);
 
-        // CASE 1 -> houses 0 to n-2
+        // CASE 1 -> use houses [0 ... n-2]
 
-        dp1[0] = nums[0];
+        dp1[1] = nums[0];
 
-        for(int i = 1; i < n-1; i++) {
+        for(int i = 2; i <= n-1; i++) {
 
-            int take = nums[i];
-
-            if(i > 1)
-                take += dp1[i-2];
-
-            int notTake = dp1[i-1];
-
-            dp1[i] = max(take, notTake);
+            dp1[i] = max(
+                dp1[i-1],
+                nums[i-1] + dp1[i-2]
+            );
         }
 
-        // CASE 2 -> houses 1 to n-1
+        // CASE 2 -> use houses [1 ... n-1]
 
         dp2[1] = nums[1];
 
-        for(int i = 2; i < n; i++) {
+        for(int i = 2; i <= n-1; i++) {
 
-            int take = nums[i];
-
-            if(i > 2)
-                take += dp2[i-2];
-
-            int notTake = dp2[i-1];
-
-            dp2[i] = max(take, notTake);
+            dp2[i] = max(
+                dp2[i-1],
+                nums[i] + dp2[i-2]
+            );
         }
 
-        return max(dp1[n-2], dp2[n-1]);
+        return max(dp1[n-1], dp2[n-1]);
     }
 };
