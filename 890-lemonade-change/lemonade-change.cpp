ff06abacc4202e2647261031fe9ccf249f3ce700
Aayushi Gupta <aayushi262004@gmail.com>
2026-06-06 14:06:04 +0530

    Time: 0 ms (100.00%) | Memory: 87.1 MB (74.02%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int n=bills.size();
-        int cnt_five,cnt_ten,cnt_twenty;
-        cnt_five=cnt_ten=cnt_twenty=0;
+        int n = bills.size();
+        int cnt5=0, cnt10=0;
         for(int i=0;i<n;i++){
-            if(bills[i]==5){
-                cnt_five++;
-            }else if(bills[i]==10){
-                cnt_ten++;
-                if(cnt_five>0){
-                cnt_five--;
+            if(bills[i] == 5){
+                cnt5++;
+            }else if(bills[i] == 10){
+                cnt10++;
+                if(cnt5>0){
+                    cnt5--;
                 }else{
                     return false;
                 }
-            }else if(bills[i]==20){
-                cnt_twenty++;
-                if(cnt_ten>0 && cnt_five>0){
-                    cnt_ten--;
-                    cnt_five--;
-                }else if(cnt_ten<=0 && cnt_five>=3){
-                    cnt_five-=3;
-                }else{
-                    return false;
-                }
-            }
+            }else{
+               if(cnt10>0 && cnt5>0){
+                cnt5--;
+                cnt10--;
+               }else if(cnt5>=3){
+                cnt5-=3;
+               }else{
+                return false;
+               }
+                      
+        }
         }
     return true;
     }
