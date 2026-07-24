4b9d3a9b162073e9340e911fc18e134c0e49f3b5
Aayushi Gupta <aayushi262004@gmail.com>
2026-07-24 18:08:35 +0530

    Time: 52 ms (35.85%) | Memory: 63.5 MB (56.63%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<vector<int>>adjRev(n);
-        vector<int>indegree(n,0);
-        //i->graph me traverse kro;
-        for(int i=0;i<n;i++){
-            for(auto it:graph[i]){
-            adjRev[it].push_back(i);
+        int n = graph.size();
+      vector<int>adjrev[n];
+      vector<int>indegree(n,0);
+      for(int i=0;i<n;i++){
+        for(auto it: graph[i]){
+            adjrev[it].push_back(i);
             indegree[i]++;
         }
-    }
-    queue<int>q;
-    vector<int>ans;
-    for(int i=0;i<n;i++){
-        if(indegree[i]==0){
+      }
+      queue<int>q;
+      vector<int>safeNodes;
+      for(int i=0;i<n;i++){
+        if(indegree[i] == 0){
             q.push(i);
         }
-    }
-    while(!q.empty()){
+      } 
+      while(!q.empty()){
         int node = q.front();
         q.pop();
-        ans.push_back(node);
-        for(auto it:adjRev[node]){
+        safeNodes.push_back(node);
+        for(auto it:adjrev[node]){
             indegree[it]--;
-            if(indegree[it]==0) q.push(it);
+            if(indegree[it] == 0)q.push(it);
         }
-    }
-sort(ans.begin(),ans.end());
-return ans;
+      } 
+      sort(safeNodes.begin(), safeNodes.end());
+    return safeNodes;
     }
 };
