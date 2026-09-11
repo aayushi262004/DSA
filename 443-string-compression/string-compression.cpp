b97096ccec919c449904fdfc18bf7fec904fbdbf
Aayushi Gupta <aayushi262004@gmail.com>
2026-09-11 14:49:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (50.66%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int n= chars.size();
-        int i= 0;
-        int index =0;
-        for(int i=0;i<n;i++){
-            int cnt =1;
-        while(i<n-1 && chars[i+1]==chars[i]){
-            i++;
-            cnt++;
-        }
-        chars[index] = chars[i];
-        index++;
-        if(cnt>1){
-            string str = to_string(cnt);
-            for(int i=0;i<str.size();i++){
-                chars[index] = str[i];
-                index++;
+
+        int n = chars.size();
+
+        string str = "";
+        int cnt = 1;
+
+        for (int i = 1; i < n; i++) {
+
+            if (chars[i] == chars[i - 1]) {
+                cnt++;
+            } else {
+
+                str += chars[i - 1];
+
+                if (cnt > 1) {
+                    str += to_string(cnt);
+                }
+
+                cnt = 1;
             }
         }
-    }
-    return index;
+
+        // process last group
+        str += chars[n - 1];
+
+        if (cnt > 1) {
+            str += to_string(cnt);
+        }
+
+        int m = str.size();
+        for (int i = 0; i < m; i++) {
+            chars[i] = str[i];
+        }
+
+        return m;
     }
 };
